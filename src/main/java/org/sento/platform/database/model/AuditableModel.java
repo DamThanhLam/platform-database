7ad2e3b8d4cae9fb40d36be97060a1e4b08b4c1f
DamThanhLam <ldamthanh@gmail.com>
2026-04-18 18:45:48 +0700

    update auditable model

--- src/main/java/org/sento/platform/database/model/AuditableModel.java
+++ src/main/java/org/sento/platform/database/model/AuditableModel.java
@@ -1,5 +1,10 @@
 package org.sento.platform.database.model;
 
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
 public abstract class AuditableModel {
     private long createdAt;
     private long updatedAt;
