5027c2e81f8f2540457f2267417aa91bed8234a7
DamThanhLam <ldamthanh@gmail.com>
2026-04-23 19:48:10 +0700

    add scan component

--- src/main/java/org/sento/platform/database/autoconfigure/ReactiveMongoDatabaseAutoConfiguration.java
+++ src/main/java/org/sento/platform/database/autoconfigure/ReactiveMongoDatabaseAutoConfiguration.java
@@ -5,11 +5,13 @@ import org.sento.platform.database.config.MongoProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
 @EnableConfigurationProperties(MongoProperties.class)
 @ConditionalOnProperty(prefix = "platform.mongodb", name = "enabled", havingValue = "true", matchIfMissing = true)
 @Import(MongoAuditingConfig.class)
+@ComponentScan("org.sento.platform.database")
 public class ReactiveMongoDatabaseAutoConfiguration {
 }
