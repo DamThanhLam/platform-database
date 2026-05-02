66acf80133bd91c6416a5806737c53ccdf060e39
DamThanhLam <ldamthanh@gmail.com>
2026-05-02 16:21:32 +0700

    add location documen

---
+++ src/main/java/org/sento/platform/database/domain/LocationDocument.java
@@ -0,0 +1,45 @@
+package org.sento.platform.database.domain;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class LocationDocument implements Serializable {
+
+    @Field("name")
+    private String name;
+
+    @Field("address")
+    private String address;
+
+    @Field("city")
+    private String city;
+
+    @Field("country")
+    private String country;
+
+    @GeoSpatialIndexed
+    @Field("coordinates")
+    private double[] coordinates;
+
+    @Builder.Default
+    @Field("type")
+    private String type = "Point";
+
+    @Field("place_id")
+    private String placeId;
+
+    @Field("accuracy")
+    private Double accuracy;
+
+    @Field("provider")
+    private String provider;
+}
